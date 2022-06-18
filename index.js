4549cc248b3df6eb6e23b41285973781e46c73f6
IDAGIZA <ruthidagiza@gmail.com>
2022-06-18 07:48:31 +0300

    commit tree 08364eee2fb11c2ea2915e7ccfc633e3cc355865

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#000000";
+dodger.style.backgroundColor = "#FF69B4";
+dodger.style.bottom = "100px";
+dodger.style.left = "100px";
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
+
+function moveDodgerRight() {
+  let rightValues = dodger.style.left.replace("px", "");
+  let rightNumber = parseInt(rightValues, 10);
+
+  if (rightNumber < 360) {
+    dodger.style.left = `${rightNumber + 5}px`;
+  }
+}
